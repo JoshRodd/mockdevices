96821f8636ab76cb32cbca7d10d03ca2d8514d85
Nate <nate.rodd@tufin.com>
2017-04-09 00:36:18 -0400

    major updates to interactive_asa.py

--- conf.py
+++ conf.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 import re
-from collections import OrderedDict, defaultdict
-import pprint
+from collections import defaultdict
+# import pprint
 
 CONFIG_STRING = ''
 OBJECT_GROUPS = defaultdict(list)
 ACLS = defaultdict(list)
 OUTPUT = []
 
+
 def asa_hash(ace):
-    return '0x{:02x}'.format(hash(ace) % pow(2,32))
+    return '0x{:02x}'.format(hash(ace) % pow(2, 32))
+
 
 with open('asa.conf') as f:
     config = f.readlines()
@@ -18,7 +20,22 @@ with open('asa.conf') as f:
     service_object_re = re.compile('\s+service-object (?P<protocol>\S+) ?((?P<type>\S+) ?((?P<operator>\S+) (?P<svc>.+)$)?)?')
     port_object_re = re.compile('\s+port-object (?P<object>.*)$')
     network_object_re = re.compile('\s+network-object (?P<object>.*)')
-    acl_re = re.compile('access-list (?P<acl>\S+) (?P<type>extended|remark (?P<remark>.*))( (?P<action>permit|deny) (?P<protocol>\d{1,3}|object-group (?P<svc_group>\S+)|[a-z]+) (?P<src>(<?P<src_network>([0-9]{1,3}\.){3}[0-9]{1,3} ([0-9]{1,3}\.){3}[0-9]{1,3})|host (?P<src_host>([0-9]{1,3}\.){3}[0-9]{1,3})|object-group (?P<src_group>\S+)|any?) (?P<dst>(?P<dst_network>([0-9]{1,3}\.){3}[0-9]{1,3} ([0-9]{1,3}\.){3}[0-9]{1,3})|host (?P<dst_host>([0-9]{1,3}\.){3}[0-9]{1,3})|object-group (?P<dst_group>\S+)|any?)( (?P<svc_type>(eq |gt |range |object-group )(?P<svc>.+)$))?)?')
+    acl_re = re.compile('access-list '
+                        '(?P<acl>\S+) '
+                        '(?P<type>extended|remark '
+                        '(?P<remark>.*))( '
+                        '(?P<action>permit|deny) '
+                        '(?P<protocol>\d{1,3}|object-group '
+                        '(?P<svc_group>\S+)|[a-z]+) '
+                        '(?P<src>(<?P<src_network>([0-9]{1,3}\.){3}[0-9]{1,3} ([0-9]{1,3}\.){3}[0-9]{1,3})|host '
+                        '(?P<src_host>([0-9]{1,3}\.){3}[0-9]{1,3})|object-group '
+                        '(?P<src_group>\S+)|any?) '
+                        '(?P<dst>(?P<dst_network>([0-9]{1,3}\.){3}[0-9]{1,3} '
+                        '([0-9]{1,3}\.){3}[0-9]{1,3})|host '
+                        '(?P<dst_host>([0-9]{1,3}\.){3}[0-9]{1,3})|object-group '
+                        '(?P<dst_group>\S+)|any?)( '
+                        '(?P<svc_type>(eq |gt |range |object-group )'
+                        '(?P<svc>.+)$))?)?')
     obj = 0
     object_group_name = ''
     for line in config:
@@ -49,7 +66,7 @@ OUTPUT.append('\
 access-list cached ACL log flows: total 0, denied 0 (deny-flow-max 4096)\n\
             alert-interval 300')
 
-for name,aces in ACLS.items():
+for name, aces in ACLS.items():
     output = []
     line = 1
     elem = 0
@@ -59,12 +76,13 @@ for name,aces in ACLS.items():
         destinations = []
         top_ace = ace['acl']
         if 'remark' in top_ace:
-            output.append(re.sub(r'access-list (\S+)',r"access-list \1 line {}".format(line),top_ace.rstrip()))
+            output.append(re.sub(r'access-list (\S+)', r"access-list \1 line {}".format(line), top_ace.rstrip()))
             line += 1
             continue
         else:
-            output.append('{} (hitcnt=0) {}'.format(re.sub(r'access-list (\S+)',r'access-list \1 line {}'.format(line),top_ace.rstrip()),asa_hash(top_ace)))
-        
+            output.append('{} (hitcnt=0) {}'.format(re.sub(r'access-list (\S+)', r'access-list \1 line {}'.format(line),
+                                                           top_ace.rstrip()), asa_hash(top_ace)))
+
         if 'object-group' not in ace['acl']:
             elem += 1
             line += 1
@@ -72,30 +90,29 @@ for name,aces in ACLS.items():
 
         if ace['svc_group']:
             for obj in OBJECT_GROUPS[ace['svc_group']]:
-                services.append([obj['protocol'],''])
+                services.append([obj['protocol'], ''])
                 if obj['svc']:
                     services[-1][1] = ' {} {}'.format(obj['operator'], obj['svc'])
                 elif obj['type']:
                     services[-1][1] = ' {}'.format(obj['type'])
         else:
-            services.append([ace['protocol'],''])
+            services.append([ace['protocol'], ''])
             if ace['svc_type']:
                 if 'object-group' in ace['svc_type']:
                     for obj in OBJECT_GROUPS[ace['svc']]:
                         services[-1][1] = ' {}'.format(obj['object'])
 
-
                 else:
-                    services[-1][1] = [' {}{}'.format(ace['svc_type'],ace['svc'])]
+                    services[-1][1] = [' {}{}'.format(ace['svc_type'], ace['svc'])]
 
         if ace['src_group']:
-            for obj in  OBJECT_GROUPS[ace['src_group']]:
+            for obj in OBJECT_GROUPS[ace['src_group']]:
                 sources.append(obj['object'])
         else:
             sources = [ace['src']]
 
         if ace['dst_group']:
-            for obj in  OBJECT_GROUPS[ace['dst_group']]:
+            for obj in OBJECT_GROUPS[ace['dst_group']]:
                 destinations.append(obj['object'])
         else:
             destinations = [ace['dst']]
@@ -104,15 +121,15 @@ for name,aces in ACLS.items():
             for source in sources:
                 for destination in destinations:
                     elem += 1
-                    output.append('  access-list {name} line {line} extended {action} {protocol} {source} {destination}{service}'\
-                                  .format(name=name,line=line,action=ace['action'],protocol=service[0],source=source,destination=destination,
+                    output.append('  access-list {name} line {line} extended {action} {protocol} {source} {destination}{service}'
+                                  .format(name=name, line=line, action=ace['action'], protocol=service[0], source=source, destination=destination,
                                           service=service[1]))
-                    output[-1] = '{} (hitcnt=0) {}'.format(output[-1],asa_hash(output[-1]))
+                    output[-1] = '{} (hitcnt=0) {}'.format(output[-1], asa_hash(output[-1]))
 
         line += 1
-    
-    output.insert(0,'access-list {}; {} elements; name hash: '.format(name,elem))
-    output[0] = '{}{}'.format(output[0],asa_hash(output[-1]))
+
+    output.insert(0, 'access-list {}; {} elements; name hash: '.format(name, elem))
+    output[0] = '{}{}'.format(output[0], asa_hash(output[-1]))
     OUTPUT.extend(output)
 
 print('\n'.join(OUTPUT))
