12092a40233ac559b086f1c2f420735024d118c4
Josh Rodd <josh@rodd.us>
2017-04-11 14:39:39 -0400

    Change quick test script

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 make install &&
-sshpass -p 'asapass' ssh -q -o StrictHostKeyChecking=no -o CheckHostIP=no -o UserKnownHostsFile=/dev/null asa@::1
+#sshpass -p 'asapass' ssh -q -o StrictHostKeyChecking=no -o CheckHostIP=no -o UserKnownHostsFile=/dev/null asa@::1
+ssh -q -o StrictHostKeyChecking=no -o CheckHostIP=no -o UserKnownHostsFile=/dev/null asa@56.0.0.1
